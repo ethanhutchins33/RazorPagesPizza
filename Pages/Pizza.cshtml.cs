ae31f83dde8417de9b71efbdfceafdf3c14bc460
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-04-16 01:59:02 +0100

    Implement Pizza Page and PageModel

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<Pizza> pizzas = new();
+
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+
         public void OnGet()
         {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+        public string IsVegan(Pizza pizza)
+        {
+            return pizza.IsVegan ? "Vegan" : "Not Vegan";
         }
     }
 }
